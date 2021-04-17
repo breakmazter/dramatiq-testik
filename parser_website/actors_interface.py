fc7a056084d5903ff77642c13750bfaf31857b15
josephmazan <80979384+josephmazan@users.noreply.github.com>
2021-04-17 20:38:52 +0300

    Update actors_interface.py

--- parser_website/actors_interface.py
+++ parser_website/actors_interface.py
@@ -1,9 +1,10 @@
 import dramatiq
 from dramatiq.brokers.rabbitmq import RabbitmqBroker
 from dramatiq.middleware import TimeLimitExceeded
-from dramatiq.results import ResultTimeout
+from dramatiq.results import ResultTimeout, Results
+from dramatiq.results.backends import RedisBackend
 
-from settings import RABBITMQ_URL
+from settings import *
 
 broker = RabbitmqBroker(url=RABBITMQ_URL)
 dramatiq.set_broker(broker)
@@ -13,6 +14,11 @@ def should_retry(retries_so_far, exception):
     return retries_so_far < 3 and not (isinstance(exception, TimeLimitExceeded) or isinstance(exception, ResultTimeout))
 
 
-@dramatiq.actor(queue_name='mazan_game_test', max_retries=3, time_limit=180000, retry_when=should_retry)
+@dramatiq.actor(store_results=False, queue_name='game_links', max_retries=3, time_limit=180000, retry_when=should_retry)
+def game_links(url):
+    pass
+
+
+@dramatiq.actor(store_results=True, queue_name='game_info', max_retries=3, time_limit=180000, retry_when=should_retry)
 def game_info(url):
     pass
